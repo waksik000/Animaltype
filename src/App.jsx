2e2b9285d21af4123db618c2f8bccce0e41fbe07
Ваше Имя <you@example.com>
2026-03-10 17:04:04 +0300

    Add Timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 
 function App() {
   const printText = 'Однажды тёплым осенним вечером, когда последние лучи заходящего солнца золотили верхушки деревьев, Иван решил прогуляться по парку. Воздух был наполнен ароматом опавшей листвы и свежести после недавнего дождя. Он шёл медленно, наслаждаясь тишиной и спокойствием, изредка поглядывая на небо, где уже начинали появляться первые звёзды.'
   const splitText = printText.split('')
   const [typedChars, setTypedChars] = useState([])
+  const [timeLimit, setTimeLimit] = useState(30)
+  const [timeLeft, setTimeLeft] = useState(30)
+  const [isRunning, setIsRunning] = useState(false)
+  const correctChars = typedChars.filter(c => c.status === 'correct').length
+  const userWPM = (correctChars / 5) / (timeLimit / 60)
+  const accuracy = typedChars.length === 0 ? 100 : (correctChars / typedChars.length) * 100
+  const errors = typedChars.filter(c => c.status === 'incorrect').length
+  useEffect(() => {
+    if (!isRunning) return
+
+    const timer = setInterval(() => {
+      setTimeLeft(prev => {if (prev <= -1) return 0
+        return prev - 1
+      })
+    },1000)
+
+    return () => clearInterval(timer)
+  }, [isRunning])
+
+  useEffect(() => {
+    if (timeLeft === 0){
+      setIsRunning(false)
+    }
+  }, [timeLeft])
   function handleKeyDown(e) {
     e.preventDefault()
+    if (timeLeft === 0) return
+
+    //time
+    if (!isRunning) {
+      setIsRunning(true)
+    }
+
     // Backspace
     if (e.key === 'Backspace') {
       setTypedChars(prev => prev.slice(0, -1))
+      return
     }
         if (e.key.length > 1) return
-    
-
     
     const currentIndex = typedChars.length
     const expectedChar = splitText[currentIndex]
@@ -22,10 +52,16 @@ function App() {
 
     setTypedChars(prev => [...prev, {char: e.key, status}])
   }
+
   return (
     <>
     <h1>Animaltype</h1>
     <p>Проверка скорости печати</p>
+    <p>Time left:{timeLeft}</p>
+    <p>WPM: {Math.round(userWPM)}</p>
+    <p>Accuracy: {Math.round(accuracy)}%</p>
+    <p>Total Words: {typedChars.length}</p>
+    <p>Errors: {errors}</p>
     <div onKeyDown = {handleKeyDown} tabIndex={0}>
       {splitText.map((ch,index) => {
         let className = ''
